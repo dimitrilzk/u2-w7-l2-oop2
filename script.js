c9d6569e87ffbfdfb41d7593668ac02516dcbdb1
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-11 14:59:21 +0200

    esercizio 1 fatto

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ class Person {
     this.nome = nome;
     this.anni = anni;
   }
+  piuVecchio(persona) {
+    if (persona.anni > this.anni) {
+      alert(persona.nome + " è più vecchio di " + this.nome);
+    }
+  }
 }
 let p1 = new Person("Antonio", 63);
 let p2 = new Person("Luca", 43);
 let p3 = new Person("Giacomo", 20);
 console.log(p2);
+p2.piuVecchio(p1);
