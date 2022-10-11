15e8418333f39f57191e6c8e4a1babaf1cbd9041
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-11 18:06:34 +0200

    1

--- pagine.js
+++ pagine.js
@@ -1,5 +1,5 @@
 class Pagina {
-  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   pageSize = 10;
   listProduct() {
     for (let i = 0; i < this.items.length; i++) {
@@ -7,5 +7,12 @@ class Pagina {
       document.getElementById("list-item").innerHTML += `<div>${item}</div>`;
     }
   }
+  maxPagina() {
+    if (this.items > this.pageSize) {
+      this.items.length === 10;
+    }
+  }
 }
 let primaPagina = new Pagina();
+let secondPagina = new Pagina();
+let terzaPagina = new Pagina();
