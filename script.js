61bd0cb494f9ef6fb1efa520284afde8cde5b8a6
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-11 14:36:57 +0200

    istanze

---
+++ script.js
@@ -0,0 +1,10 @@
+class Person {
+  constructor(nome, anni) {
+    this.nome = nome;
+    this.anni = anni;
+  }
+}
+let p1 = new Person("Antonio", 63);
+let p2 = new Person("Luca", 43);
+let p3 = new Person("Giacomo", 20);
+console.log(p2);
